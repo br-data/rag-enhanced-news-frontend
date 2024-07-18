cbf12525794decb51b503af453e765dd83b3c6b7
Steffen Kühne <stekhn@gmail.com>
2024-07-18 16:17:07 +0200

    Add enrichment style, update response demo

--- src/pages/Details/Details.tsx
+++ src/pages/Details/Details.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {
+  Icon,
   mergeStyleSets,
   PrimaryButton,
   Shimmer,
@@ -20,15 +21,27 @@ const Details: React.FunctionComponent = () => {
   const { id } = useParams<RouteParams>();
   const wire = wires.find((item) => item.id === id);
 
-  interface Data {
+  interface GenericData {
     [key: string]: any;
+  }
+
+  interface Response {
+    id: string;
+    headline: string;
+    enhanced_info: string;
+    questions?: Questions[] | null;
+  }
+
+  interface Questions {
+    question: string;
+    links?: string[] | null;
   }
 
   const url =
     "https://br24-geschichten-function.azurewebsites.net/api/Repeater?code=hwgMw21CRcLF_6DZve8hFWqKnzDst2NzrdAwztrObka4AzFuo0_gKg%3D%3D";
-  const body: Data = mockResponse;
+  const body: GenericData = mockResponse;
 
-  const { data, loading, error, executeFetch } = useFetch<Data>(url, {
+  const { data, loading, error, executeFetch } = useFetch<Response>(url, {
     method: "POST",
     body,
   });
@@ -45,7 +58,7 @@ const Details: React.FunctionComponent = () => {
   });
 
   return (
-    <div>
+    <div style={{ marginBottom: "2rem" }}>
       <h1 className={classNames.header}>{wire?.headline}</h1>
       <p>{wire?.created_at}</p>
       <div dangerouslySetInnerHTML={{ __html: wire?.article_html || "" }} />
@@ -57,22 +70,69 @@ const Details: React.FunctionComponent = () => {
         />
       )}
       {error && <p>Error: {error.message}</p>}
-      {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
+      {data && (
+        <>
+          <section
+            style={{
+              margin: "2rem 0",
+              paddingLeft: "15px",
+              borderLeft: "5px solid #0078d4",
+            }}
+          >
+            <h3>More info</h3>
+            <p dangerouslySetInnerHTML={{ __html: data.enhanced_info || "" }} />
+          </section>
+          <section>
+            <h3>Questions and sources</h3>
+            <ul
+              style={{
+                padding: "0",
+                listStyle: "inside",
+              }}
+            >
+              {data.questions?.map((question, index) => (
+                <li key={index}>
+                  {question.question} (
+                  {question.links?.map((link, index) => (
+                    <a href={link} target="_blank" rel="noreferrer">
+                      Quelle {index + 1}
+                    </a>
+                  ))}
+                  )
+                </li>
+              ))}
+            </ul>
+          </section>
+          <section style={{
+              margin: "2rem 0",
+            }}>
+            <em>
+              <Icon iconName="Warning" style={{ marginRight: "0.5rem" }} />
+              Please make sure to double check the generated content and
+              sources.
+            </em>
+          </section>
+        </>
+      )}
       {/* <Stack className={classNames.selectors}>
             <Shimmer />
             <Shimmer />
             <Shimmer />
             <Shimmer />
           </Stack> */}
-      <Stack>
-        <Stack.Item align="end">
-          <PrimaryButton
-            text="Primary"
-            onClick={executeFetch}
-            allowDisabledFocus
-          />
-        </Stack.Item>
-      </Stack>
+      {!!!data && (
+        <Stack>
+          <Stack.Item align="center">
+            <PrimaryButton
+              style={{ marginTop: "1rem" }}
+              text="Generate enrichments"
+              onClick={executeFetch}
+              iconProps={{ iconName: "CompassNW" }}
+              allowDisabledFocus
+            />
+          </Stack.Item>
+        </Stack>
+      )}
     </div>
   );
 };
