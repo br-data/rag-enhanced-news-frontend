7ebe193cd3de1b7b19a9b8241a6ded498f0538cb
Steffen Kühne <stekhn@gmail.com>
2024-07-18 16:55:33 +0200

    Add delay, move styles to classes

--- src/pages/Details/Details.tsx
+++ src/pages/Details/Details.tsx
@@ -1,12 +1,5 @@
-import React from "react";
-import {
-  Icon,
-  mergeStyleSets,
-  PrimaryButton,
-  Shimmer,
-  Spinner,
-  Stack,
-} from "@fluentui/react";
+import React, { useState } from "react";
+import { mergeStyleSets, PrimaryButton, Spinner, Stack } from "@fluentui/react";
 import { useParams } from "react-router-dom";
 
 import wires from "../../data/wires.json";
@@ -37,6 +30,12 @@ const Details: React.FunctionComponent = () => {
     links?: string[] | null;
   }
 
+  const [sleeping, setSleeping] = useState(false);
+
+  const sleep = (ms: number): Promise<void> => {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  };
+
   const url =
     "https://br24-geschichten-function.azurewebsites.net/api/Repeater?code=hwgMw21CRcLF_6DZve8hFWqKnzDst2NzrdAwztrObka4AzFuo0_gKg%3D%3D";
   const body: GenericData = mockResponse;
@@ -46,55 +45,60 @@ const Details: React.FunctionComponent = () => {
     body,
   });
 
+  const handleClick = async () => {
+    setSleeping(true);
+    await sleep(5000);
+    executeFetch();
+    setSleeping(false);
+  };
+
   const classNames = mergeStyleSets({
-    header: {
-      margin: ".5em 0",
+    container: { marginBottom: "2rem" },
+    header: { margin: ".5em 0" },
+    loading: { marginTop: "2rem" },
+    enhancedInfo: {
+      margin: "2rem 0",
+      paddingLeft: "15px",
+      borderLeft: "5px solid #0078d4",
     },
-    selectors: {
-      "& > .ms-Shimmer-container": {
-        margin: "2px 0",
-      },
-    },
+    list: { padding: "0", listStyle: "inside" },
+    warning: { margin: "2rem 0", backgroundColor: "#f4f4f4", padding: "1rem" },
   });
 
   return (
-    <div style={{ marginBottom: "2rem" }}>
+    <div className={classNames.container}>
       <h1 className={classNames.header}>{wire?.headline}</h1>
       <p>{wire?.created_at}</p>
       <div dangerouslySetInnerHTML={{ __html: wire?.article_html || "" }} />
-      {loading && (
-        <Spinner
-          label="Searching for possible text enrichments"
-          ariaLive="assertive"
-          labelPosition="right"
-        />
+      {(sleeping || loading) && (
+        <Stack className={classNames.loading}>
+          <Spinner
+            label="Searching for possible text enrichments"
+            ariaLive="assertive"
+            labelPosition="right"
+          />
+        </Stack>
       )}
       {error && <p>Error: {error.message}</p>}
       {data && (
         <>
-          <section
-            style={{
-              margin: "2rem 0",
-              paddingLeft: "15px",
-              borderLeft: "5px solid #0078d4",
-            }}
-          >
+          <section className={classNames.enhancedInfo}>
             <h3>More info</h3>
             <p dangerouslySetInnerHTML={{ __html: data.enhanced_info || "" }} />
           </section>
           <section>
             <h3>Questions and sources</h3>
-            <ul
-              style={{
-                padding: "0",
-                listStyle: "inside",
-              }}
-            >
+            <ul className={classNames.list}>
               {data.questions?.map((question, index) => (
-                <li key={index}>
+                <li key={`question-${index}`}>
                   {question.question} (
                   {question.links?.map((link, index) => (
-                    <a href={link} target="_blank" rel="noreferrer">
+                    <a
+                      key={`link-${index}`}
+                      href={link}
+                      target="_blank"
+                      rel="noreferrer"
+                    >
                       Quelle {index + 1}
                     </a>
                   ))}
@@ -103,30 +107,21 @@ const Details: React.FunctionComponent = () => {
               ))}
             </ul>
           </section>
-          <section style={{
-              margin: "2rem 0",
-            }}>
+          <section className={classNames.warning}>
             <em>
-              <Icon iconName="Warning" style={{ marginRight: "0.5rem" }} />
-              Please make sure to double check the generated content and
+              ⚠️ Please make sure to double-check the generated content and
               sources.
             </em>
           </section>
         </>
       )}
-      {/* <Stack className={classNames.selectors}>
-            <Shimmer />
-            <Shimmer />
-            <Shimmer />
-            <Shimmer />
-          </Stack> */}
-      {!!!data && (
+      {!data && !loading && !sleeping && (
         <Stack>
           <Stack.Item align="center">
             <PrimaryButton
               style={{ marginTop: "1rem" }}
               text="Generate enrichments"
-              onClick={executeFetch}
+              onClick={handleClick}
               iconProps={{ iconName: "CompassNW" }}
               allowDisabledFocus
             />
